f73b73e4ce57712d36de94bf1327ab3c1a62be93
Asus <jema.salvador.up@phinmaed.com>
2024-01-28 10:36:05 +0800

    splash screen added

--- app/src/main/java/com/example/devlearnhub/MainActivity.kt
+++ app/src/main/java/com/example/devlearnhub/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.devlearnhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        val delayMillis = 2000L
+        val intent = Intent(this, Login_Activity::class.java)
+
+        android.os.Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        }, delayMillis)
     }
 }
