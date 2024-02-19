c8146974f6a95c267e06a18424e0c9e28532b349
Asus <jema.salvador.up@phinmaed.com>
2024-02-19 11:49:40 +0800

    0 added and 2 modified

--- app/src/main/java/com/example/devlearnhub/Java_Activity.kt
+++ app/src/main/java/com/example/devlearnhub/Java_Activity.kt
@@ -1,11 +1,28 @@
 package com.example.devlearnhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.devlearnhub.databinding.LayoutJavaActivityBinding
+import com.example.devlearnhub.databinding.LayoutPythonActivityBinding
 
 class Java_Activity : AppCompatActivity() {
+    private lateinit var binding: LayoutJavaActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_java_activity)
+        binding = LayoutJavaActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnBackToHome.setOnClickListener {
+            val intent = Intent(this, Initial_Activity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnNextLearn.setOnClickListener {
+            val intent = Intent(this, com.example.devlearnhub.module.Java_Module1::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
