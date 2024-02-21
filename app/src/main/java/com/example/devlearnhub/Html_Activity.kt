35c4fc6ad18a893c89af32d71e3c09f5c1be6f59
Asus <jema.salvador.up@phinmaed.com>
2024-02-21 16:37:27 +0800

    9 added and 2 modified

---
+++ app/src/main/java/com/example/devlearnhub/Html_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.devlearnhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Html_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_html_activity)
+    }
+}
