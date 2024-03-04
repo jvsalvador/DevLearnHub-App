1c76f97633bde5c513aad592a17e8c1812bf0a1a
Marlos Ramos <arsa.ramos.up@phinmaed.com>
2024-03-05 01:25:51 +0800

    34 added and 4 modified

---
+++ app/src/main/java/com/example/devlearnhub/module/Mysql_Module2_3.kt
@@ -0,0 +1,12 @@
+package com.example.devlearnhub.module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.devlearnhub.R
+
+class Mysql_Module2_3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mysql_module2_3)
+    }
+}
---
+++ app/src/main/java/com/example/devlearnhub/module/Mysql_Module3_5.kt
@@ -0,0 +1,12 @@
+package com.example.devlearnhub.module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.devlearnhub.R
+
+class Mysql_Module3_5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mysql_module3_5)
+    }
+}
