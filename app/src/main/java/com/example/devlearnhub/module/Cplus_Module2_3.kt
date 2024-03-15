97cc59c0c516924cbfe67889cd29a4d95c322428
Marlos Ramos <arsa.ramos.up@phinmaed.com>
2024-03-15 13:07:17 +0800

    34 added and 4 modified

---
+++ app/src/main/java/com/example/devlearnhub/module/Cplus_Module2_3.kt
@@ -0,0 +1,21 @@
+package com.example.devlearnhub.module
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.devlearnhub.R
+
+class Cplus_Module2_3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.cplus_module2_3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
